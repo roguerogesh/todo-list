684375b1955408505524c9529f99f3b4cc90f00e
roguerogesh <rogesh062246@gmail.com>
2023-11-01 09:55:29 +0000

    solution

---
+++ src/components/SimpleTodos/index.js
@@ -0,0 +1,72 @@
+import {Component} from 'react'
+
+import TodoItem from './components/TodoItem'
+
+import './index.css'
+
+const initialTodosList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+class SimpleTodos extends Component {
+  state = {todoDetailsList: initialTodosList}
+
+  deleteItem = id => {
+    const {todoDetailsList} = this.state
+    const filteredTodoData = todoDetailsList.filter(each => each.id !== id)
+
+    this.setState({todoDetailsList: filteredTodoData})
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="small-container">
+          <h1 className="heading">Simple Todos</h1>
+          <ul className="list-container">
+            {initialTodosList.map(eachList => (
+              <TodoItem
+                todoList={eachList}
+                key={eachList.id}
+                deleteItem={this.deleteItem}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SimpleTodos
---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+const TodoItem = props => {
+  const {todoList} = props
+  const {id, title} = todoList
+  onDelete = () => {
+    deleteItem(id)
+  }
+
+  return (
+    <li className="list-item">
+      <p className="title">{title}</p>
+      <button type="button" className="button" onClick={onDelete}>
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
